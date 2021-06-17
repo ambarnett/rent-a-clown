f453deb6a3cb7f05d128a6dd1923247daa70a33e
ambarnett <andy.barnett09@gmail.com>
2021-06-17 14:11:31 -0500

    Made it horrifying... Hover over the submit button...

--- src/scripts/ClownRental.js
+++ src/scripts/ClownRental.js
@@ -3,7 +3,10 @@ import { Reservations } from "./reservations.js"
 
 export const ClownRental = () => {
     return `
-    <h1>The fun house</h1>
+    <header class="header">
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-02.belfasttelegraph.co.uk%2Fopinion%2Fcolumnists%2Fgail-walker%2Farticle35119200.ece%2F2f708%2FAUTOCROP%2Fw620h342%2F2016-10-11_opi_25389998_I1.JPG&f=1&nofb=1" class="logo" />
+        <h1><u>The FUN house</u></h1>
+    </header>    
     <section class="reservationForm">
         ${reservationForm()}
     </section>
